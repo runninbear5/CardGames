b191e1b0efbf6c7f8f07a84fef850be5dff52e7f
runninbear5 <blakelieber@gmail.com>
2017-10-25 07:46:17 -0700

    added quit to game choser

--- src/GameChoser.java
+++ src/GameChoser.java
@@ -4,11 +4,12 @@ import java.util.Scanner;
 public class GameChoser {
 	public static void main(String[] args) throws IOException {
 		String blackJack = "blackjack";// setting game names
+		String quit = "quit";
 		Scanner sc = new Scanner(System.in);// init scanner
 		boolean play = true;// allows while loop to run at least once
 		boolean cont = true;
 		while (play) {
-			System.out.println("Welcome to game center, would you like to play: BlackJack");
+			System.out.println("Welcome to game center, would you like to play: 'BlackJack', or 'quit'");
 			String gameChoice = sc.nextLine().toLowerCase();// looking for user input
 			if (gameChoice.equals(blackJack)) {// checks if equal to blackjack to then play blackjack
 				System.out.println("Welcome to Black Jack, where the money is fake but the fun is real");
@@ -16,6 +17,8 @@ public class GameChoser {
 					BlackJack bj = new BlackJack();
 					cont = bj.playBlackJack();
 				}
+			}else if (gameChoice.equals(quit)) {// checks if equal to blackjack to then play blackjack
+					play=false;
 			}
 		}
 	}
